2adaf2ee024f72dca49670560de5f6845b306c73
Théo Fuhrmann <theo@Theos-MacBook-Pro.local>
2025-05-29 11:11:42 +0200

    removed shoulders

--- tools/body_parts_map.py
+++ tools/body_parts_map.py
@@ -1,7 +1,6 @@
 body_parts_map = {
     "left_arm": [5, 7, 9],
     "right_arm": [6, 8, 10],
-    "shoulders": [5, 6],
     "head": [0, 1, 2, 3, 4],
     "face": list(range(11, 79)),
     "left_hand": list(range(79, 100)),
